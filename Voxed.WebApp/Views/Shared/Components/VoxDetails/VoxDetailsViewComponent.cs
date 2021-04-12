b5d28e147bf64a03baae4c0c321f3437fc29e426
Lucas <lucasjavierquintanilla@gmail.com>
2021-04-12 20:50:24 +0000

    Creado vox view component

---
+++ Voxed.WebApp/Views/Shared/Components/VoxDetails/VoxDetailsViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Core.Data.Repositories;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Voxed.WebApp.Views.Shared.Components.VoxDetails
+{
+    public class VoxDetailsViewComponent : ViewComponent
+    {
+        private readonly IVoxedRepository voxedRepository;
+
+        public VoxDetailsViewComponent(
+                IVoxedRepository voxedRepository
+            )
+        {
+            this.voxedRepository = voxedRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(IEnumerable<Vox> voxs)
+        {
+            return View(voxs);
+        }
+    }
+}
