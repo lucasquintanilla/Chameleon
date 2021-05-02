cddb93234c0f119ab8db67629e154d0634369c22
Lucas <lucasjavierquintanilla@gmail.com>
2021-05-02 05:11:10 +0000

    agregada entitdad notification

---
+++ Core.Entities/Notification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public enum NotificationType
+    {
+        Comment, Response
+    }
+
+    public class Notification
+    {
+        public Guid Id { get; set; }
+        public Guid? UserId { get; set; }
+        public Guid? CommentId { get; set; }
+        public NotificationType Type { get; set; }
+        public int Count { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public DateTimeOffset UpdatedOn { get; set; }
+        public virtual Vox Vox { get; set; }
+        public virtual Comment Comment { get; set; }
+    }
+}
