74061c87396387417a8bbc944ae2ae7d73f111c8
lucasquintanilla <lquintanilla@originsw.com>
2021-05-20 20:32:24 -0300

    limpieza general contundente

--- Voxed.WebApp/Models/CommentFormViewModel.cs
+++ Voxed.WebApp/Models/CommentFormViewModel.cs
@@ -1,10 +1,7 @@
 ﻿using Core.Validations;
 using Microsoft.AspNetCore.Http;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Voxed.WebApp.Models
 {
--- Voxed.WebApp/Views/Shared/Components/CommentForm/CommentFormViewComponent.cs
+++ Voxed.WebApp/Views/Shared/Components/CommentForm/CommentFormViewComponent.cs
@@ -1,22 +1,14 @@
-﻿using Core.Data.Repositories;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-//using Voxed.WebApp.Data;
 using Voxed.WebApp.Models;
 
 namespace Voxed.WebApp.Views.Shared.Components.CommentForm
 {
     public class CommentFormViewComponent : ViewComponent
     {
-        //private readonly VoxedContext _context;
-        private readonly IVoxedRepository voxedRepository;
-
-        public CommentFormViewComponent(IVoxedRepository voxedRepository)
+        public CommentFormViewComponent()
         {
-            this.voxedRepository = voxedRepository;
         }
 
         public async Task<IViewComponentResult> InvokeAsync(Guid voxID, Guid userID)
