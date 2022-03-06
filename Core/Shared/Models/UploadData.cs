9313bd9d2969f0e95a7ecf9a069efe1053ea2026
Lucas Quintanilla <lquintanilla@repay.com>
2022-03-06 00:02:54 +0000

    clean code

--- Core/Shared/Models/UploadData.cs
+++ Core/Shared/Models/UploadData.cs
@@ -9,5 +9,10 @@ namespace Core.Shared.Models
         public string Preview { get; set; }
         public string Extension { get; set; }
         public string ExtensionData { get; set; }
+
+        public bool HasData()
+        {
+            return Preview != null && Extension != null && ExtensionData != null;
+        }
     }
 }
